9d281ccf929bd92e49a36cafc2a1d75025616c23
bas <bas.groot@basement.nl>
2022-04-07 12:30:35 +0200

    Add GET

--- js/demo.js
+++ js/demo.js
@@ -90,6 +90,28 @@
 
     function sendRequest() {
 
+        function getParameters(id) {
+            const value = document.getElementById(id).value;
+            return value.trim().replace(/\r/g, "").split(/\n/g);
+        }
+
+        function getQueryParameters(parameters) {
+            let result = "";
+            parameters.forEach(function (parameter) {
+                const pos = parameter.indexOf("=");
+                const key = parameter.substring(0, pos);
+                const value = parameter.substring(pos + 1);
+                if (value !== "" && value !== "0") {
+                    result += (
+                        result === ""
+                        ? "?"
+                        : "&"
+                    ) + key + "=" + encodeURIComponent(value);
+                }
+            });
+            return result;
+        }
+
         function getPostBody() {
             try {
                 return JSON.parse(document.getElementById("idRequestBody").value);
@@ -128,7 +150,9 @@
                 "Authorization": "Bearer " + tokenObject.access_token
             }
         };
+        const pathParameters = getParameters("idPathParameters");
         let postBody;
+        let queryParameters = getQueryParameters(getParameters("idQueryParameters"));
         if (method === "POST" || method === "PATCH") {
             postBody = getPostBody(postBody);
             yamlUtils.removeEmptyValues(postBody);
@@ -138,7 +162,7 @@
                 setupStreamer();
             }
         }
-        fetch(url, fetchData).then(function (response) {
+        fetch(url + queryParameters, fetchData).then(function (response) {
             const correlationInfo = "X-Correlation header (for troubleshooting with Saxo): " + response.headers.get("X-Correlation");
             if (response.ok) {
                 response.json().then(function (responseJson) {
@@ -185,6 +209,16 @@
     }
 
     function populateScreen() {
+
+        function populateTextArea(id, value) {
+            const textArea = document.getElementById(id);
+            if (value === "") {
+                textArea.style.display = "none";
+            } else {
+                textArea.value = value;
+            }
+        }
+
         const httpMethod = yamlUtils.getHttpMethod();
         const endpoint = yamlUtils.getEndpoint();
         // Get method from the URL:
@@ -193,11 +227,11 @@
         document.getElementById("idEndpoint").innerText = endpoint;
 
         // Get the path parameters from the yaml file:
-        document.getElementById("idPathParameters").value = yamlUtils.getPathParameters(endpoint, httpMethod);
+        populateTextArea("idPathParameters", yamlUtils.getParameters(endpoint, httpMethod, "path"));
         // Get the query parameters from the yaml file:
-        document.getElementById("idQueryParameters").value = yamlUtils.getQueryParameters(endpoint, httpMethod);
+        populateTextArea("idQueryParameters", yamlUtils.getParameters(endpoint, httpMethod, "query"));
         // Get the request object from the yaml file:
-        document.getElementById("idRequestBody").value = yamlUtils.getRequestBody(endpoint, httpMethod);
+        populateTextArea("idRequestBody", yamlUtils.getRequestBody(endpoint, httpMethod));
     }
 
     setupEvents();
