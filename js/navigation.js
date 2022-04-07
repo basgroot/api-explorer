30d6b4c6dc10b9528cd02d8d2e7ef5623afc50d6
jaabsaxo <jaab@saxobank.com>
2022-04-07 12:22:21 +0200

    checkpoint

--- js/navigation.js
+++ js/navigation.js
@@ -1,6 +1,47 @@
 var coll = document.getElementsByClassName("left-nav-collapsible");
 var i;
 
+function createTag(name) {
+  let b = document.createElement('button');
+  b.className = "left-nav-collapsible";
+  b.innerText = name;
+  return b;
+}
+
+function createLink(title, url) {
+  let d = document.createElement('div');
+  d.className = "content"
+
+  d.innerHTML = "<p>x</p>"
+
+  return d;
+}
+
+fetch("https://basgroot.github.io/api-explorer/config/navigation.json").then(r => r.json()).then(navigation => {
+  //console.log(navigation);
+
+  let d = document.getElementById("left-nav-content");
+  
+  for (let tagName in navigation) {
+    //console.log(tagName)
+
+    var tag = createTag(tagName)
+
+    for (let title in navigation[tagName]) {
+      //console.log(title)
+      let url = navigation[tagName][title]
+      //console.log(endpoint)
+
+      let link = createLink(title, url)
+      tag.appendChild(link);
+    }
+  }
+
+  d.appendChild(tag);
+
+
+})
+
 console.log("test test test");
 
 for (i = 0; i < coll.length; i++) {
